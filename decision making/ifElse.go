36b073fe1844bf705d17f1eada09bd3c8a4bd23f
AIsokjonov <abdurauf.isokjonov@gmail.com>
2021-01-08 12:04:52 +0900

    if else

---
+++ decision making/ifElse.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+
+func main() {
+	var myName string = "James"
+	if (myName == "James") {
+		fmt.Println("Hello, Jamesy Boy!")
+	} else {
+		fmt.Println("hey, ", myName, "!")
+	}
+}
