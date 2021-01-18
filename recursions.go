e107c01cd0ad4ddd9d9dbfecb92331196893377b
AIsokjonov <abdurauf.isokjonov@gmail.com>
2021-01-19 00:10:04 +0900

    more on recursions

--- recursions.go
+++ recursions.go
@@ -1,13 +1,32 @@
 package main
 import "fmt"
 
-func fact(n int) int {
-	if n == 0 {
-		return 1
-	}
-	return n * fact(n - 1)
+// fibonacci
+func fibonacci(n int) (res int) {
+  if n <= 1 {
+    res = 1
+  } else {
+    res = fibonacci(n - 1) + fibonacci(n - 2)
+  }
+  return
+}
+
+// factorial
+func factorial(n uint64) (res uint64) {
+  if n <= 1 {
+    return 1
+  }
+  res = n * factorial(n - 1)
+  return
 }
 
 func main() {
-	fmt.Println(fact(4))
+  result := 0
+  for i := 0; i <= 10; i++ {
+    result = fibonacci(i)
+    fmt.Printf("Fibonacci(%d) is: %d\n", i, result)
+  }
+
+  // factorial
+  fmt.Printf("\nFactorial of %d is: %d\n", 5, factorial(5))
 }
