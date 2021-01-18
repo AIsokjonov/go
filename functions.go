f2e9d6c989a86ea258ddba26b97169ccd9b24c63
AIsokjonov <abdurauf.isokjonov@gmail.com>
2021-01-18 22:28:41 +0900

    variadic functions

--- functions.go
+++ functions.go
@@ -16,20 +16,44 @@ func greet(name string) string {
   return "Hello " + name
 }
 
+func Multiply(a, b int, reply *int) {
+  *reply = a * b
+}
+
+func myPrint(a ...int) {
+  if len(a) == 0 {
+    return
+  }
+
+  for _, v := range a {
+    fmt.Printf("The number is: %d\n", v)
+  }
+}
+
 func main() {
   // calling function inside function
   fmt.Printf("Value: %d\n", sum(arithmetic(20, 10)))
 
-  fmt.Print("Enter you name: ")
-  var name string
-  fmt.Scanf("%s", &name)
+  //fmt.Print("Enter you name: ")
+  //var name string
+  //fmt.Scanf("%s", &name)
 
   // assign a function to a variable
-  sayHello := greet(name)
-  fmt.Println(sayHello)
+  //sayHello := greet(name)
+  //fmt.Println(sayHello)
 
   // blank identifier example
   fmt.Println("\nArithmetic operations")
   a1, _, a3 := arithmetic(3, 5) // the second variable is the blank identifier
   fmt.Printf("Addition: %d\nMultiplication: %d\n", a1, a3)
+
+  // changing an outside variable
+  n := 0 // int variable
+  reply := &n // saves the memory address of n == pointer
+  Multiply(10, 5, reply)
+  fmt.Println("\nMultiply:", *reply)
+  fmt.Println("Multiply:", n)
+
+  // variadic functions
+  myPrint(1,23, 134, 41, 42, 0, -23)
 }
