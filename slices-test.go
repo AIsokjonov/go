cdbc3b1aaa080f468da829c1a83b133f1e4807ce
AIsokjonov <abdurauf.isokjonov@gmail.com>
2021-01-20 10:58:59 +0900

    multidimensionality

---
+++ slices-test.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func slice_fib(n int) []int {
+  slice := make([]int, n)
+  slice[0], slice[1] = 1, 1
+  for i:=2; i < n; i++ {
+    slice[i] = slice[i-1] + slice[i-2]
+  }
+  return slice
+}
+
+func main() {
+  fmt.Println(slice_fib(10))
+}
