f2f8378bf364a5b5e6b2bbf0551dcd0bf9ffc3d2
AIsokjonov <abdurauf.isokjonov@gmail.com>
2021-01-20 16:07:46 +0900

    copy and append slices

---
+++ copyAppendSlice.go
@@ -0,0 +1,21 @@
+package main
+import "fmt"
+
+func main() {
+	// copy a slice into another slice
+	sl := []int{1,2,3}
+	sl_copy := make([]int, 10)
+	n := copy(sl_copy, sl)
+
+	fmt.Printf("Original slice: %v\n", sl)
+	fmt.Printf("Copy slice: %v\n", sl_copy)
+	fmt.Printf("Number of copied elements: %v\n", n)
+
+	// append a slice into another slice
+	sl2 := []int{2345,234,-234}
+	fmt.Println("\nAppend slice")
+	fmt.Printf("Original slice: %v\n", sl2)
+
+	sl2 = append(sl2, 15, 54)
+	fmt.Printf("Appended slice: %v\n", sl2)
+}
