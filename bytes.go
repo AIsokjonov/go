db99840d6a1e1239567649e96d0e308764dabcf9
AIsokjonov <abdurauf.isokjonov@gmail.com>
2021-01-20 11:43:49 +0900

    bytes

---
+++ bytes.go
@@ -0,0 +1,17 @@
+package main
+import (
+	"fmt"
+	"bytes"
+)
+
+func main() {
+	var buf bytes.Buffer
+	fmt.Printf("Buffer: %v\n", &buf)
+
+	buf.WriteString("SBV")
+	fmt.Printf("Buffer: %v\n", &buf)
+
+	buf.WriteString("ABV")
+	fmt.Printf("Buffer: %v\n", &buf)
+	fmt.Printf("Altogether: %v\n", buf.String())
+}
