245b4c2b2eaad9032199e1f41b53f24d73e0fb4f
AIsokjonov <abdurauf.isokjonov@gmail.com>
2021-01-21 08:12:50 +0900

    search strings

--- search.go
+++ search.go
@@ -6,19 +6,20 @@ import (
 )
 
 func main() {
-	// searching int
-	sl := []float64{1.2, -0.73, 4.42, 5.42, 23.53}
-	x := 3.5
-	fmt.Printf("Original: %v\n", sl)
+	// searching strings
+	sl := []string{"James","Bob","Robert","Mark","Alex","Mike"}
+	x := "Robert"
 
 	start := time.Now()
-
-	fmt.Printf("Search item: %v\n", x)
-	sort.Float64s(sl)
-	fmt.Printf("Sorted slice: %v\n", sl)
-	fmt.Printf("Searching float64: %v\n", sort.SearchFloat64s(sl, x))
+	
+	fmt.Printf("Original: %v\n", sl)
+	sort.Strings(sl)
+	fmt.Printf("Sorted: %v\n", sl)
+	
+	fmt.Printf("\nSearch item: %v\n", x)
+	fmt.Printf("Search Result: %v\n", sort.SearchStrings(sl, x))
 
 	end := time.Now()
 	delta := end.Sub(start)
-	fmt.Printf("It took %v to complete this task\n", delta)
+	fmt.Printf("\nIt took %v to complete this task\n", delta)
 }
