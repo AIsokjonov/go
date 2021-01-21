3ad68ba81c96ac8aaae082a8e47ebe8244327204
AIsokjonov <abdurauf.isokjonov@gmail.com>
2021-01-21 09:06:19 +0900

    garbage collection

---
+++ garbageCollection.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+
+var arr []byte = []byte{'a','b','a','a','a','c','d','e','f','g'}
+
+func main() {
+	sl := make([]byte, len(arr))
+	i := 0
+	tmp := byte(0)
+
+	fmt.Printf("Before: %v\n", arr)
+	for _, v := range arr {
+		if v != tmp {
+			sl[i] = v
+			i++
+		}
+		tmp = v
+	}
+	fmt.Printf("After: %v\n", sl)
+}
