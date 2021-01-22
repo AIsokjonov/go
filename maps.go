f9604789fcf13b1ebce1b54fd3f1c49013335f93
AIsokjonov <abdurauf.isokjonov@gmail.com>
2021-01-22 12:11:50 +0900

    more on maps

--- maps.go
+++ maps.go
@@ -2,9 +2,40 @@ package main
 import "fmt"
 
 func main() {
-	students := map[int][]int{
-		1: {1,2,3},
-		2: {4,5,6},
+	// map declaration
+	students := map[int]string{}
+	
+	// maps with integer slices as values
+	processes := map[int][]int{}
+
+	// maps with string slices as values
+	pros := map[int][]string{}
+
+
+	// initialization
+	students = map[int]string{
+		1: "Abdurauf",
+		2: "Bob",
+		3: "Jasur",
 	}
-	fmt.Printf("Map with slices: %v\n", students)
+
+	processes = map[int][]int{
+		1: {124,542,53465},
+		2: {235,524,213},
+		3: {756, 875,7452},
+	}
+
+	pros = map[int][]string{
+		1: {"mysqld", "systemctl","code"},
+		2: {"xcode", "terminal","chrome"},
+		3: {"code-helper","docker"},
+	}
+
+	// check the existence of key-value item
+	_, isPresent := students[5]
+	
+	fmt.Println(students)
+	fmt.Println(processes)
+	fmt.Println(pros)
+	fmt.Printf("Existence: %v\n", isPresent)
 }
