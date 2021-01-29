93a75dc6b469aace0d4a9bb6ff83bd60e7be732a
AIsokjonov <abdurauf.isokjonov@gmail.com>
2021-01-29 14:21:36 +0900

    size of struct
    get size of struct with unsafe package \nunsafe.Sizeof(&Node{})

--- src/structs/structs.go
+++ src/structs/structs.go
@@ -1,11 +1,26 @@
-package structs
+package main
+import "fmt"
+import "unsafe"
 
-type Framework struct { name string }
-type Language struct { name string }
-type DB struct { name string }
+type SinglyLinkedList struct {
+	data float64 // this field can be any type
+	next *SinglyLinkedList
+}
 
-type IT struct {
-	L *Language
-	F *Framework
-	D *DB
+type DoublyLinkedList struct {
+	pr *DoublyLinkedList // predecessor
+	data float64 // this field can be any type
+	su *DoublyLinkedList // successor
+}
+
+type BinaryTree struct {
+	left *BinaryTree
+	data float64 // this field can be any type
+	right *BinaryTree
+}
+
+func main() {
+	fmt.Println("Size of Singly Linked List: ", unsafe.Sizeof(SinglyLinkedList{324.32, &SinglyLinkedList{}}))
+	fmt.Printf("Size of Double Linked List: %v\n", unsafe.Sizeof(DoublyLinkedList{&DoublyLinkedList{}, 34.42, &DoublyLinkedList{}}))
+	fmt.Printf("Size of Binary Tree: %v\n", unsafe.Sizeof(BinaryTree{&BinaryTree{}, 12.312, &BinaryTree{}}))
 }
