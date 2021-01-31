7a11565714b3646275f7fd00e13c2c8f3843f650
AIsokjonov <abdurauf.isokjonov@gmail.com>
2021-01-31 21:37:02 +0900

    methods vs functions

---
+++ src/functions/functions.go
@@ -0,0 +1,17 @@
+package functions
+import (
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func Abs(v Vertex) float64 {
+	return math.Sqrt(v.X * v.X + v.Y * v.Y)
+}
+
+func Scale(v *Vertex, f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
