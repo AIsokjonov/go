d52dca48905225f2a482d9473eb39f14c08bbb63
AIsokjonov <abdurauf.isokjonov@gmail.com>
2021-02-01 11:55:15 +0900

    calling methods on values and pointers

--- src/methods/methods.go
+++ src/methods/methods.go
@@ -1,61 +1,22 @@
 package main
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
-// methods with struct types
-type Vertex struct {
-	X, Y float64
-}
+type List []int
 
-func (v Vertex) Abs() float64 {
-	return math.Sqrt(v.X * v.X + v.Y * v.Y)
-}
+func (l List) Len() int { return len(l) }
 
-// pointer receiver
-func (v *Vertex) Scale(f float64) {
-	v.X = v.X * f
-	v.Y = v.Y * f
-}
-
-// above methods as a function
-func ScaleFunc(v *Vertex, f float64) {
-	v.X = v.X * f
-	v.Y = v.Y * f
-}
-
-// methods with non-struct types
-type MyFloat float64
-
-func (m MyFloat) Sum() float64 {
-	if m < 0 {
-		return float64(-m)
-	}
-	return float64(m)
-}
+func (l *List) Append(val int) { *l = append(*l, val) }
 
 func main() {
-	// methods for struct types
-	v := Vertex{3, 4}
-	fmt.Println(v.Abs())
+	// a bare value
+	var lst List
+	lst.Append(1)
+	fmt.Printf("%v (len: %d)\n", lst, lst.Len())
+	fmt.Printf("Type: %T\n\n", lst)
 
-	// methods for non-struct types
-	f := MyFloat(-3.02345)
-	fmt.Println(f.Sum())
-
-	// pointer receiver
-	v.Scale(7)
-	fmt.Println(v.Abs())
-
-	// methods vs functions difference
-	v2 := Vertex{3, 4}
-	v2.Scale(3)
-	ScaleFunc(&v2, 10)
-
-	p2 := &Vertex{3, 4}
-	p2.Scale(3)
-	ScaleFunc(p2, 10)
-
-	fmt.Println(v2, p2)
+	// a pointer value
+	plst := new(List)
+	plst.Append(2)
+	fmt.Printf("%v (len: %d)\n", plst, lst.Len())
+	fmt.Printf("Type: %T\n", plst)
 }
