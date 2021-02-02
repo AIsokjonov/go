86c9386931d07d2c447230773cc5135114f688e1
AIsokjonov <abdurauf.isokjonov@gmail.com>
2021-02-02 11:43:03 +0900

    embed functionality in a type

--- src/interfaces/interfaces.go
+++ src/interfaces/interfaces.go
@@ -1,23 +1,37 @@
 package main
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
-type Point struct {
-	x, y float64
+type Log struct {
+	msg string
 }
 
-func (p *Point) Abs() float64 {
-	return math.Sqrt(p.x * p.x + p.y * p.y)
-}
-
-type NamedPoint struct {
-	Point
-	name string
+type Customer struct {
+	Name string
+	log *Log
 }
 
 func main() {
-	n := &NamedPoint{Point{3, 4}, "Pythagoras"}
-	fmt.Println(n.Abs())
+	c := new(Customer)
+	c.Name = "Borack Obama"
+	c.log = new(Log)
+	c.log.msg = "1 - Yes we can!"
+	fmt.Println(c.Log())
+
+	// shorter
+	c2 := &Customer{"Donal Trump", &Log{"Make America great Again!"}}
+	fmt.Println(c2.log)
+	c2.Log().Add("2 - After me, the world will be a better place!")
+	fmt.Println(c2.Log())
+}
+
+func (l *Log) Add(s string) {
+	l.msg += "\n" + s
+}
+
+func (l *Log) String() string {
+	return l.msg
+}
+
+func (c *Customer) Log() *Log {
+	return c.log
 }
