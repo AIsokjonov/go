bf4f02867c7aaf37ac5dc48b9b7b1f4baa3c177e
AIsokjonov <abdurauf.isokjonov@gmail.com>
2021-02-03 10:13:34 +0900

    more on interfaces

--- src/interfaces/interfaces.go
+++ src/interfaces/interfaces.go
@@ -1,21 +1,39 @@
-package interfaces
+package main
+import (
+	"fmt"
+	"math"
+)
 
-type Log struct {
-	msg string
+type Abser interface {
+	Abs() float64
 }
 
-type Customer struct {
-	Name string
-	Log
+type myFloat float64
+
+func (f myFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
 }
 
-// func main() {
-// 	c := &Customer{"Donald Trump", Log{"Make America Great Again!"}}
-// 	fmt.Println(c.msg)
-// 	c.Log.Add("by Donald Trump")
-// 	fmt.Println(c.msg)
-// }
+type Vertex struct {
+	X, Y float64
+}
 
-func (l *Log) Add(s string) {
-	l.msg += "\n" + s
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X * v.X + v.Y * v.Y)
+}
+
+func main() {
+	var a Abser
+	f := myFloat(-math.Sqrt2)
+	v := Vertex{3, 4}
+
+	a = f
+	a = &v
+
+	a = v
+
+	fmt.Println(a.Abs())
 }
