f936a3339cfdd415ad3a44605eb7865267a01f1d
AIsokjonov <abdurauf.isokjonov@gmail.com>
2021-02-03 14:46:42 +0900

    interface values

--- src/interfaces/interfaces.go
+++ src/interfaces/interfaces.go
@@ -9,11 +9,24 @@ type T struct {
 	S string
 }
 
-func (t T) M() {
+func (t *T) M() {
 	fmt.Println(t.S)
 }
 
+type F float64
+
+func (f F) M() {
+	fmt.Println(f)
+}
+
 func main() {
-	var i I = T{"Hello"}
+	var i I
+
+	i = &T{"Hello"}
+	describe(i)
 	i.M()
+}
+
+func describe(i I) {
+	fmt.Printf("(%v, %T)\n", i, i)
 }
